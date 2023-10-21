01012c1d4266d19c415ef66cd0bf070503f14c72
JonathanDiz <jdiazolivares007@gmail.com>
2023-10-21 12:15:41 -0300

    refactor: reestructurar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent,
     PaginationComponent,
-    FooterComponent
+    FooterComponent,
+    PokemonSearchComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/pokemon-search/pokemon-search.component.ts
+++ src/app/components/pokemon-search/pokemon-search.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { PokemonService } from '../../service/pokemon.service';
+import { PokemonService } from '../../service/pokemon-search.service';
 
 @Component({
-  selector: 'app-pokemon-list',
-  templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.sass']
+  selector: 'app-pokemon-search',
+  templateUrl: './pokemon-search.component.html',
+  styleUrls: ['./pokemon-search.component.sass']
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonSearchComponent implements OnInit{
   nombrePokemon: string = '';
   pokemonList: any[] = [];
   listaPokemon: any[] = [];
   currentPage: number = 1; // Página actual
   itemsPerPage: number = 20; // Cantidad de Pokémon por página
   totalPages: number = 0; // Total de páginas disponibles
-
+  
   constructor(private pokemonService: PokemonService) { }
-
+  
   ngOnInit() {
     this.cargarListaPokemon();
   }
-
+  
   buscarPokemon() {
     this.listaPokemon = [];
-
+  
     if (this.nombrePokemon) {
       this.pokemonService.getPokemonDetails(this.nombrePokemon).subscribe((data) => {
         this.listaPokemon.push({
@@ -32,22 +32,22 @@ export class PokemonListComponent implements OnInit {
       });
     }
   }
-
+  
   cargarListaPokemon() {
     this.pokemonService.getPokemonList().subscribe((data) => {
       this.pokemonList = data.results;
       this.totalPages = Math.ceil(data.count / this.itemsPerPage);
     });
-
+  
     // Carga la primera página de Pokémon inicialmente
     this.loadPokemonPage(1);
   }
-
+  
   changePage(page: number) {
     this.currentPage = page;
     this.loadPokemonPage(this.currentPage);
   }
-
+  
   loadPokemonPage(page: number) {
     // Implementa la lógica para cargar Pokémon de la página indicada
   }
@@ -58,11 +58,12 @@ export class PokemonListComponent implements OnInit {
       this.loadPokemonPage(this.currentPage);
     }
   }
-
+  
   nextPage() {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
       this.loadPokemonPage(this.currentPage);
     }
   }
-}
+  }
+  
