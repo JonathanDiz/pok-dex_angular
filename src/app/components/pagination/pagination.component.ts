c420c220770e470775a95362250e3fb9454c3719
JonathanDiz <jdiazolivares007@gmail.com>
2023-10-23 22:21:15 -0300

    refactor: filtros service

--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PokemonFilterService } from '../../service/pokemon-filter.service';
 
 @Component({
   selector: 'app-pagination',
@@ -11,25 +12,37 @@ export class PaginationComponent implements OnInit {
   @Input() currentPage: number = 1;
   @Input() totalPages: number = 1;
   @Input() itemsPerPage: number = 2000;
+  @Input() alphabet: string[] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
   @Output() pageChange = new EventEmitter<number>();
   visiblePokemonList: any[] = [];
 
-  constructor(private httpClient: HttpClient) { }
+  private originalPokemonList: any[] = [];
+  private filteredPokemonList: any[] = [];
+  private currentFilter: string | null = null;
+
+  constructor(
+    private httpClient: HttpClient,
+    private pokemonFilterService: PokemonFilterService
+  ) { }
 
   ngOnInit() {
     // Inicialmente, carga los Pokémon de la primera página
     this.loadVisiblePokemonList(this.currentPage);
   }
 
-  loadVisiblePokemonList(page: number): void {
+  loadVisiblePokemonList(page: number, filterByLetter?: string): void {
     const itemsPerPage = this.itemsPerPage;
     const offset = (page - 1) * itemsPerPage;
 
     // Construye la URL para obtener los Pokémon de la página actual
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${offset}`;
+    let apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=${itemsPerPage}&offset=${offset}`;
+
+    if (filterByLetter) {
+      apiUrl += `&name=${filterByLetter}%`; // Filtra por nombre que comienza con la letra
+    }
 
     // Realiza una solicitud HTTP para obtener los Pokémon
-    this.httpClient.get(apiUrl).subscribe((data: any) => {
+    this.httpClient.get<any>(apiUrl).subscribe((data) => {
       const results = data.results;
       const visiblePokemonList: any[] = [];
 
@@ -53,6 +66,29 @@ export class PaginationComponent implements OnInit {
   private getPokemonId(url: string): number {
     const parts = url.split('/');
     return parseInt(parts[parts.length - 2], 10);
+  }
+
+  loadPokemonPage(page: number) {
+    // Usar la lista filtrada o completa según el filtro actual
+    const sourceList = this.currentFilter ? this.filteredPokemonList : this.pokemonList;
+
+    const startIndex = (page - 1) * this.itemsPerPage;
+    const endIndex = Math.min(startIndex + this.itemsPerPage, sourceList.length);
+
+    this.visiblePokemonList = sourceList.slice(startIndex, endIndex);
+  }
+
+  // Método para filtrar Pokémon por letra
+  applyFilterByLetter(letter: string) {
+    this.currentFilter = letter;
+    this.filteredPokemonList = this.pokemonFilterService.filterByLetter(this.pokemonList, letter);
+    this.loadPokemonPage(1);
+  }
+
+  clearFilter() {
+    this.currentFilter = null;
+    this.filteredPokemonList = [];
+    this.loadPokemonPage(1);
   }
 
   previousPage() {
--- src/app/service/pokemon-filter.service.ts
+++ src/app/service/pokemon-filter.service.ts
@@ -6,4 +6,18 @@ import { Injectable } from '@angular/core';
 export class PokemonFilterService {
 
   constructor() { }
+
+  // Filtrar por letra inicial del nombre
+  filterByLetter(pokemonList: any[], letter: string): any[] {
+    return pokemonList.filter((pokemon) =>
+      pokemon.name.startsWith(letter.toUpperCase())
+    );
+  }
+
+  // Filtrar por tipo de Pokémon
+  filterByType(pokemonList: any[], type: string): any[] {
+    return pokemonList.filter((pokemon) =>
+      pokemon.types.includes(type.toUpperCase())  
+    );
+  }
 }
